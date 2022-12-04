57460927d64989135fdf33b0a0fc84cce840a030
Kyr Nastahunin <111767711+knasta1@users.noreply.github.com>
2022-12-04 10:46:20 -0600

    Updated the requirements.txt file

--- Python/app.py
+++ Python/app.py
@@ -8,7 +8,6 @@ from google_helper import GoogleHelper
 from rake_keyword_extract import RakeExtract
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-
 app = Flask(__name__)
 CORS(app)
 
@@ -21,23 +20,23 @@ def get_keywords(place_id):
     # if none returned then it's a bad request
     if reviews_list is None:
         return "No reviews found for the link", 400
-    # extract only the keywords, and not their bias
+    # extract the data to return in proper json format
     keywords_list = []
     sid_obj = SentimentIntensityAnalyzer()
     for review in reviews_list:
+        # extract keywords from each review
         extracted_keywords = RakeExtract(review)
         for keyword in extracted_keywords:
-   
+            # get the sentiment of the keyword
             sentiment_dict = sid_obj.polarity_scores(keyword[0])
             predicted_sentiment = "Neutral"
-            if sentiment_dict['compound'] >= 0.10 :
-               predicted_sentiment = 'Positive'
- 
-            elif sentiment_dict['compound'] <= - 0.10 :
+            if sentiment_dict['compound'] >= 0.10:
+                predicted_sentiment = 'Positive'
+            elif sentiment_dict['compound'] <= -0.10:
                 predicted_sentiment = 'Negative'
-            
+            # add the keyword and sentiment to the final list to be returned
             keywords_list.append({"keyword": keyword[0], "importance": keyword[1], "sentiment": predicted_sentiment})
 
     js = {'keywords': keywords_list}
+    # return response
     return Response(json.dumps(js), mimetype='application/json')
-
