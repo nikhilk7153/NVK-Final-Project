6520e0ffbc0d4831de8b1d4a282672bba771903a
vineetrc <37272048+vineetrc@users.noreply.github.com>
2022-12-05 19:57:40 -0600

    Added comments

--- Python/rake_keyword_extract.py
+++ Python/rake_keyword_extract.py
@@ -12,9 +12,12 @@ numpy.set_printoptions(threshold=sys.maxsize)
 
 nltk.download('punkt')
 
+# https://www.researchgate.net/publication/227988510_Automatic_Keyword_Extraction_from_Individual_Documents
 
+# This function is an Implementation of the Rapid Automatic Keyword Extraction (RAKE) Algorithm
 def RakeExtract(text):
     
+    # doing some preprocessing with contractions
     expanded_words = []   
     for word in text.split():
       # using contractions.fix to expand the shortened words
@@ -28,7 +31,8 @@ def RakeExtract(text):
     
     text_tokens = nltk.word_tokenize(text)
     sub_tokens = []
-
+    
+    # delimiting tokenized sentence based on stop words
     for word in text_tokens:
         if word in STOP_WORDS or word in string.punctuation:
             sub_tokens.append("-")
@@ -38,7 +42,9 @@ def RakeExtract(text):
     spacetokens = []
 
     res = ""
-
+    
+    # more delimiation edge case handling
+    # joining words together into phrases that are adjacent to each other in delimited sentence
     for i in range(0, len(sub_tokens) - 1):
         if sub_tokens[i] == "-":
             if res != "":
@@ -54,12 +60,16 @@ def RakeExtract(text):
     if sub_tokens[-1] != "-":
         res += sub_tokens[-1]
     spacetokens.append(res)
-
+    
+    # Keeping track of count of a specific word in wrdcnt dictionary
     wrdcnt = {}
     for i in sub_tokens:
         if i != "-":
             wrdcnt[i] = wrdcnt.get(i, 0) + 1
 
+    # This function initializes the vocabulary of the text
+    # Returns a word dictionary initalized to a unique index for every word
+    # This dictionary will store the index of the word in the 2-D Graph Adjacency List
     def getinitinfo(tokens):
 
         wordset = set()
@@ -73,12 +83,13 @@ def RakeExtract(text):
             worddict[i] = cnt
             cnt += 1
         return worddict
-
+    
+    # create graph to store counts of coocurrences of words within the same phrase in text
     dic = getinitinfo(sub_tokens)
     N = len(dic)
     graph = np.zeros((N, N))
 
-    # ["hello world space", "-", "food"]
+    # Update graph with the adjacent words that appear together
     for i in spacetokens:
         if i != "-":
             spl = i.split()
@@ -89,7 +100,10 @@ def RakeExtract(text):
                         graph[dic[spl[j]]][dic[spl[i]]] += 1
 
     results = []
-
+    
+    # calculate Degree of word node in graph and the count of word in text
+    # divide degree/wrdcnt to get imortance score for each word
+    # caclulate important phrases score by adding score for each word in phrase
     degree = np.sum(graph, axis=0)
     for i, (k, v) in enumerate(dic.items()):
         results.append((k, degree[v] / wrdcnt[k]))
@@ -106,7 +120,8 @@ def RakeExtract(text):
                 if x[0] == y:
                     score += x[1]
         keywords.add((word, score))
-
+        
+    # sort phrases by summation of their words score in increasing order
     keywords = list(keywords)
     keywords.sort(key=lambda x: x[1])
     keywords = keywords[::-1]
